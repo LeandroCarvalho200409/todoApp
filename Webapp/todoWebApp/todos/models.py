9bfddc2aaf43ce41f565c782665e352a79b76bba
Leandro200409 <78601676+Leandro200409@users.noreply.github.com>
2022-08-17 22:22:54 +0200

    Finished creating the database connection by creating two classes to represent the tables in the database. Created some first templates.

--- Webapp/todoWebApp/todos/models.py
+++ Webapp/todoWebApp/todos/models.py
@@ -1,3 +1,18 @@
+from cgitb import text
+from datetime import date
 from django.db import models
 
 # Create your models here.
+
+class user(models.Model):
+    username = models.TextField(unique=True)
+    password = models.TextField()
+    name = models.TextField()
+    email = models.TextField()
+
+
+class todo(models.Model):
+    name = models.TextField()
+    text = models.TextField()
+    date = models.DateField()
+    user_fk = models.ForeignKey(user, on_delete=models.CASCADE)
